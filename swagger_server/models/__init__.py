515fdc34a56cbd562e2be1b4d4b13ee099701ae3
Quinten <qvdpost@gmail.com>
2023-02-08 13:13:32 +0100

    initial commit

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.grade_record import GradeRecord
+from swagger_server.models.one_of_student_grade_records_items import OneOfStudentGradeRecordsItems
+from swagger_server.models.student import Student
