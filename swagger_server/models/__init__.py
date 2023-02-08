793bcfb55514f5132842866e9289d1b3ee83efc3
Quinten <qvdpost@gmail.com>
2023-02-08 13:27:50 +0100

    new version

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -4,5 +4,4 @@
 from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.grade_record import GradeRecord
-from swagger_server.models.one_of_student_grade_records_items import OneOfStudentGradeRecordsItems
 from swagger_server.models.student import Student
